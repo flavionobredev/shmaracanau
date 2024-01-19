b97f5430c0903ce2ec9451f387969dd33f055580
Flávio Nobre <flavionobredev@gmail.com>
2024-01-19 13:21:19 -0300

    feat: add customization feature tool

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <main class="main-default">
+    <slot />
+  </main>
+</template>
+<style scoped>
+.main-default {
+  @apply min-h-svh bg-[#e84411] pb-20;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const buttons = [
+  {
+    text: "🌟 Inscrições Renascer (Participantes)",
+    to: "https://docs.google.com/forms/d/e/1FAIpQLSfsUhuuwULS2qTilNYAgoIUV_C39NaGkpdMgfjvxvAtTn1yUQ/viewform",
+    target: "_blank",
+  },
+  {
+    text: "Personalizar foto do Perfil",
+    to: "/tools/profile-customization",
+    target: "",
+  },
+  {
+    text: "Conheça nosso CEV",
+    to: "https://maps.app.goo.gl/EHbuBNbgBJwBXvuX6?g_st=ic",
+    target: "_blank",
+  },
+];
+</script>
+<template>
+  <section class="mx-auto md:w-1/2 xl:w-1/4">
+    <div></div>
+    <div>
+      <ul class="grid grid-flow-row gap-3">
+        <NuxtLink
+          v-for="btn in buttons"
+          class="text-center bg-white rounded text-[#e84411] py-3 px-6 font-extrabold w-full"
+          :to="btn.to"
+          :target="btn.target"
+          >{{ btn.text }}
+        </NuxtLink>
+      </ul>
+    </div>
+  </section>
+</template>
+<style scoped>
+.btn-home {
+  @apply text-center bg-white rounded;
+}
+</style>
