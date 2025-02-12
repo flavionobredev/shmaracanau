974ac79d29dfde5be43466746f3fef93552b4dc8
Flávio Nobre <flavionobredev@gmail.com>
2025-02-12 01:52:35 -0300

    chore: add post build script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2025-02-12",
   app: {
     head: {
       meta: [
@@ -41,5 +42,4 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css", "~/assets/css/tailwind.css"],
   modules: ["@nuxtjs/tailwindcss", "nuxt-cron"],
-
 });
